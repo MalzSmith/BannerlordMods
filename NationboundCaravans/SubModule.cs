a064ea7de983e0a225eabccf81bdf5a3ace67833
MalzSmith <malzi@malzi.hu>
2024-05-23 19:45:21 +0200

    Workshop version

--- NationboundCaravans/SubModule.cs
+++ NationboundCaravans/SubModule.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using HarmonyLib;
+using MCM.Abstractions.Base.Global;
+using MCM.Abstractions.FluentBuilder;
+using MCM.Common;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.CampaignBehaviors;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 
 
@@ -12,6 +18,10 @@ namespace NationboundCaravans;
 
 public class SubModule : MBSubModuleBase
 {
+    public static bool OnlyEffectPlayers { get; set; } = true; // "Much elegant, very clean
+
+    private FluentGlobalSettings _settings;
+    
     protected override void OnSubModuleLoad()
     {
         base.OnSubModuleLoad();
@@ -24,12 +34,43 @@ public class SubModule : MBSubModuleBase
         // Replace the method that decides the next caravan destination
         harmony.Patch(originalMethod, new(Patches.Prefix));
     }
+
+    public override void OnAfterGameInitializationFinished(Game game, object starterObject)
+    {
+        base.OnAfterGameInitializationFinished(game, starterObject);
+        
+        InformationManager.DisplayMessage(new("NationboundCaravans loaded", Colors.Green));
+        
+        if (game.GameType is not Campaign)
+        {
+            return;
+        }
+        
+        _settings = BaseSettingsBuilder.Create("NationboundCaravans", "Nationbound Caravans")!
+            .SetFormat("xml")
+            .SetFolderName(string.Empty)
+            .SetSubFolder(string.Empty)
+            .WithoutDefaultPreset()
+            .CreateGroup("Main", b => b
+                .AddBool("playersOnly", "Only effect player caravans",
+                    new ProxyRef<bool>(() => OnlyEffectPlayers, v => OnlyEffectPlayers = v), _ => { }))
+            .CreatePreset("defaultPreset", "Default", b => b
+                .SetPropertyValue("playersOnly", true))
+            .BuildAsGlobal();
+        
+        _settings.Register();
+    }
+
+    public override void OnGameEnd(Game game)
+    {
+        base.OnGameEnd(game);
+        
+        _settings.Unregister();
+    }
 }
 
 public static class Patches
 {
-    private static Dictionary<Hero, List<IFaction>> AllowedFactions = new();
-    
     // _caravanLastHomeTownVisitTime
     private static readonly FieldInfo CaravanLastHomeTownVisitTimeField;
 
@@ -54,10 +95,10 @@ public static class Patches
     {
         CaravanLastHomeTownVisitTimeField = typeof(CaravansCampaignBehavior).GetField("_caravanLastHomeTownVisitTime",
             BindingFlags.NonPublic | BindingFlags.Instance);
-        
+
         PreviouslyChangedCaravanTargetsDueToEnemyOnWayField = typeof(CaravansCampaignBehavior).GetField(
             "_previouslyChangedCaravanTargetsDueToEnemyOnWay", BindingFlags.NonPublic | BindingFlags.Instance);
-        
+
         GetTradeScoreForTownMethod = typeof(CaravansCampaignBehavior).GetMethod("GetTradeScoreForTown",
             BindingFlags.NonPublic | BindingFlags.Instance);
     }
@@ -69,11 +110,11 @@ public static class Patches
     public static bool Prefix(MobileParty caravanParty, bool distanceCut, ref Town __result,
         CaravansCampaignBehavior __instance)
     {
-        if (!caravanParty.Owner.IsHumanPlayerCharacter)
+        if (SubModule.OnlyEffectPlayers && !caravanParty.Owner.IsHumanPlayerCharacter)
         {
             return true;
         }
-        
+
         // Get used fields with reflection
         var _caravanLastHomeTownVisitTime =
             (Dictionary<MobileParty, CampaignTime>)CaravanLastHomeTownVisitTimeField.GetValue(__instance);
@@ -87,7 +128,7 @@ public static class Patches
         float caravanFullness = caravanParty.ItemRoster.TotalWeight / (float)caravanParty.InventoryCapacity;
 
         _caravanLastHomeTownVisitTime.TryGetValue(caravanParty, out var lastHomeVisitTimeOfCaravan);
-        foreach (var allTown in Town.AllTowns)
+        foreach (var allTown in Town.AllTowns.Where(t => t.MapFaction.Id == caravanParty.Owner.MapFaction.Id))
         {
             if (allTown.Owner.Settlement != caravanParty.CurrentSettlement
                 && !allTown.IsUnderSiege
